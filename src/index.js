1dff6a75a38479a0be4eeaaa13d0b96de07e224d
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-10-22 07:40:51 -0500

    Create slide moving logic function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "./styles.css"
+
+let slideIndex = 0; // Initial index
+const nextButton = document.querySelector(".next-button");
+const backButton = document.querySelector(".back-button");
+let slides = document.querySelectorAll(".slide");
+
+function showSlide(num) {
+    if (num >= slides.length) { // If index number exceeds slides length, wrap to beginning of slides
+        slideIndex = 0;
+    }
+
+    else if (num < 0) { // If index gets negative, wraps to end of slides
+        slideIndex = slides.length - 1;
+    }
+
+    else {
+        slideIndex = num;
+    }
+
+    slides.forEach(slide => {
+        slide.style.display = "none"; // Sets all slides to be invisible
+    })
+
+    slides[slideIndex].style.display = "block"; // Sets first index of images to visible block display
+}
+
+nextButton.addEventListener("click", () => { // Moves forward in slides length
+    showSlide(slideIndex + 1);
+})
+
+backButton.addEventListener("click", () => { // Moves back in slides length
+    showSlide(slideIndex - 1);
+})
+
+showSlide(slideIndex);
